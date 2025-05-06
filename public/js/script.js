d6b80593f298659d2368f1dc5edfa1debb98cade
mark-well <lacemerto05@gmail.com>
2025-05-06 09:39:45 +0800

    fix: only the login page changes the background image

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,19 @@
 
+let loginPage = document.getElementById("login-body");
 let changeImageDelaySec = 8 * 1000;
 let slidesPath = "/assets/images/slides/";
 let slides = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 let currentImage = 0;
 
 window.onload = function() {
-    setInterval(changeBackgroundImage, changeImageDelaySec);
+    if(loginPage != null) {
+        setInterval(changeBackgroundImage, changeImageDelaySec);
+    } 
 }
 
 // Changes the background image of the login page
 function changeBackgroundImage() {
-    document.body.style.backgroundImage = `url('${slidesPath + slides[currentImage]}')`;
-    console.log("Current image: " + currentImage);
+    loginPage.style.backgroundImage = `url('${slidesPath + slides[currentImage]}')`;
 
     if(currentImage < slides.length-1) {
         currentImage += 1;
